a779671e5af67f943e70c808626ade21df414657
sniperbhiya12 <aarubhakar302@gmail.com>
2025-05-26 15:30:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/sachinopboy2/ShizukaMusicBot",
+    "https://github.com/sniperbhiya12/Sniparsmartboy",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
